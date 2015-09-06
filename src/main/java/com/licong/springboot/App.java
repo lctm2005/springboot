77447a39d293ca44d9bc8c650a9c97eeab37de4b
lctm2005 <licong2002@163.com>
2015-09-06 23:03:19 +0800

    更 新

--- src/main/java/com/licong/springboot/App.java
+++ src/main/java/com/licong/springboot/App.java
@@ -1,12 +1,7 @@
 package com.licong.springboot;
 
-import com.licong.springboot.web.listener.ApplicationStartupListener;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.event.ApplicationStartedEvent;
-import org.springframework.context.ApplicationListener;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -14,12 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @SpringBootApplication // same as @Configuration @EnableAutoConfiguration @ComponentScan
-@EnableAutoConfiguration
 public class App {
 
     public static void main(String[] args) throws Exception {
         SpringApplication springApplication = new SpringApplication(App.class);
-        springApplication.addListeners(new ApplicationStartupListener());
         springApplication.run(App.class, args);
     }
 }
