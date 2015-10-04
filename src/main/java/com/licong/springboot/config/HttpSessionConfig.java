671eb1f9866e58035592a67a40a12ef5cdbf12bd
lctm2005 <licong2002@163.com>
2015-10-04 21:26:59 +0800

    删除学习用的profile示例，改为更加实用的home、work的profile分类，应对家与公司的不同环境

---
+++ src/main/java/com/licong/springboot/config/HttpSessionConfig.java
@@ -0,0 +1,10 @@
+package com.licong.springboot.config;
+
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+/**
+ * Created by lctm2005 on 2015/10/4.
+ */
+@EnableRedisHttpSession
+public class HttpSessionConfig {
+}
